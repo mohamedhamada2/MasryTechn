e96118f91f73365d06c223e95e8212d7a7607fd3
mohamedhamada2 <mohamedhamada2024@gmail.com>
2019-09-29 21:16:58 +0200

    mohamed

---
+++ app/src/main/java/com/nglah/masrytechn/view/user_data/User_Data_Activity.java
@@ -0,0 +1,18 @@
+package com.nglah.masrytechn.view.user_data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nglah.masrytechn.R;
+
+public class User_Data_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user__data_);
+
+
+    }
+}
