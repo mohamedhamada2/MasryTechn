2e85b87512365bbf0b962c7f37aec596246087fe
mohamedhamada2 <mohamedhamada2024@gmail.com>
2019-10-04 21:58:33 +0200

    mohamed

---
+++ app/src/main/java/com/nglah/masrytechn/view/Car_Info/Car_info.java
@@ -0,0 +1,16 @@
+package com.nglah.masrytechn.view.Car_Info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nglah.masrytechn.R;
+
+public class Car_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_info);
+    }
+}
---
+++ app/src/main/java/com/nglah/masrytechn/view/Pick_Time/Pick_time.java
@@ -0,0 +1,26 @@
+package com.nglah.masrytechn.view.Pick_Time;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.nglah.masrytechn.R;
+
+public class Pick_time extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_time);
+    }
+
+    public void Now(View view) {
+    }
+
+    public void Cancle(View view) {
+    }
+
+    public void Order(View view) {
+    }
+}
