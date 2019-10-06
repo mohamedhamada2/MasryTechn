f06091aab27e8034e1dcc6a1ca567c58f31a1c5c
mohamedhamada2 <mohamedhamada2024@gmail.com>
2019-10-06 21:19:25 +0200

    mohamed

---
+++ app/src/main/java/com/nglah/masrytechn/view/Accepted_Info/Accepted_User_Info.java
@@ -0,0 +1,16 @@
+package com.nglah.masrytechn.view.Accepted_Info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nglah.masrytechn.R;
+
+public class Accepted_User_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accepted__user__info);
+    }
+}
